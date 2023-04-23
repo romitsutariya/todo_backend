60995ae124eea66839009f54650ddb0f75e4cf87
Romit Sutariya <romitsutariya@gmail.com>
2023-04-23 21:02:27 +0530

    resolved merge conflict

--- model/user.js
+++ model/user.js
@@ -1,14 +1,9 @@
+const mongoose = require("mongoose");
 const { Schema, model } = require("mongoose");
 
-<<<<<<< HEAD
 const userSchema = new mongoose.Schema({
   firstname: { type: String, default: null },
   lastname: { type: String, default: null },
-=======
-const userSchema = new Schema({
-  first_name: { type: String, default: null },
-  last_name: { type: String, default: null },
->>>>>>> 4afe0e20799243561224af0b0c9990f0d6f85127
   email: { type: String, unique: true },
   password: { type: String },
   token: { type: String },
